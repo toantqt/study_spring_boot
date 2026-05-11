96bbe942666405aadab7e6001e68034ffcf7f16a
Tyler Tran <tyler.tran@beyondedge.co>
2026-05-11 09:17:16 +0700

    init src

---
+++ src/main/java/com/example/study_spring_boot/StudySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/study_spring_boot/StudySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
